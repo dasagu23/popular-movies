ba86722eebe161bea94ab4711c7994c59fc09e87
Scott Deschene <sdeschene@thelevelup.com>
2015-12-14 21:56:46 -0500

    Refactor trailers task

--- app/src/main/java/com/deschene/popularmovies/fragment/DetailFragment.java
+++ app/src/main/java/com/deschene/popularmovies/fragment/DetailFragment.java
@@ -4,11 +4,9 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,18 +22,9 @@ import com.deschene.popularmovies.R;
 import com.deschene.popularmovies.adapter.TrailerAdapter;
 import com.deschene.popularmovies.data.MoviesContract;
 import com.deschene.popularmovies.model.Movie;
+import com.deschene.popularmovies.network.AbsractFetchReviewsTask;
+import com.deschene.popularmovies.network.AbsractFetchTrailersTask;
 import com.squareup.picasso.Picasso;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -45,6 +34,7 @@ public class DetailFragment extends Fragment {
     private Switch mFavoriteSwitch;
     private Movie mMovie;
     private String[] mTrailers;
+    private String[] mReviews;
     private TrailerAdapter mTrailerAdapter;
 
     /**
@@ -77,7 +67,9 @@ public class DetailFragment extends Fragment {
         }
 
         if (savedInstanceState == null) {
-            updateTrailers();
+            fetchTrailers();
+            fetchReviews();
+            //            fetchReviews();
         }
 
         return rootView;
@@ -162,121 +154,41 @@ public class DetailFragment extends Fragment {
         });
     }
 
-    private void updateTrailers() {
-        new FetchTrailersTask().execute(mMovie.getId(), getString(R.string.themoviesdb_api_key));
+    private void fetchTrailers() {
+        new FetchMovieTrailersTask()
+                .execute(mMovie.getId(), getString(R.string.themoviesdb_api_key));
+    }
+
+    private void fetchReviews() {
+//        new FetchMovieReviewsTask()
+//                .execute(mMovie.getId(), "reviews", getString(R.string.themoviesdb_api_key));
     }
 
     /**
      * AsyncTask for fetching the movies from themoviedb.
      */
-    public class FetchTrailersTask extends AsyncTask<String, Void, String[]> {
-
-        private final String LOG_TAG = FetchTrailersTask.class.getSimpleName();
-
-        @Override
-        protected String[] doInBackground(final String... params) {
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String trailersJsonString = null;
-
-            try {
-                final Uri.Builder builder = new Uri.Builder();
-                builder.scheme("https").authority("api.themoviedb.org").appendPath("3")
-                        .appendPath("movie").appendPath(params[0]).appendPath("videos")
-                        .appendQueryParameter("api_key", params[1]);
-
-                final URL url = new URL(builder.toString());
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                final InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                trailersJsonString = buffer.toString();
-            } catch (final IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // Can't parse data if it wasn't successfully retrieved.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try {
-                return getTrailerDataFromJson(trailersJsonString);
-            } catch (final JSONException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
-            }
-
-            // Won't happen unless there is a json error
-            return null;
-        }
+    public class FetchMovieTrailersTask extends AbsractFetchTrailersTask {
 
         @Override
         protected void onPostExecute(final String[] data) {
             super.onPostExecute(null);
 
             mTrailers = data;
-            // Update Trailers view
             onTrailersLoaded();
         }
     }
 
     /**
-     * Converts the json string of movie data to an array of String trailers.
-     *
-     * @param trailersJsonStr the string of json
-     * @return a String array of trailers
-     * @throws JSONException if parsing string fails
+     * AsyncTask for fetching the movies from themoviedb.
      */
-    public String[] getTrailerDataFromJson(final String trailersJsonStr) throws JSONException {
-        // These are the names of the JSON objects that need to be extracted.
-        final String OWM_RESULTS = "results";
-        final String OWM_KEY = "key";
+    public class FetchMovieReviewsTask extends AbsractFetchReviewsTask {
 
-        final JSONObject trailersJson = new JSONObject(trailersJsonStr);
-        final JSONArray trailersArray = trailersJson.getJSONArray(OWM_RESULTS);
+        @Override
+        protected void onPostExecute(final String[] data) {
+            super.onPostExecute(null);
 
-        final String[] trailers = new String[trailersArray.length()];
-
-        for (int i = 0; i < trailersArray.length(); i++) {
-            final JSONObject movieObject = trailersArray.getJSONObject(i);
-
-            trailers[i] = movieObject.getString(OWM_KEY);
+            mReviews = data;
+            // onReviewsLoaded();
         }
-
-        return trailers;
     }
 }
---
+++ app/src/main/java/com/deschene/popularmovies/network/AbsractFetchReviewsTask.java
@@ -0,0 +1,121 @@
+package com.deschene.popularmovies.network;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * AsyncTask for fetching movie reviews and trailers.
+ */
+public class AbsractFetchReviewsTask extends AsyncTask<String, Void, String[]> {
+
+    private final String LOG_TAG = AbsractFetchReviewsTask.class.getSimpleName();
+
+    @Override
+    protected String[] doInBackground(final String... params) {
+        // params[0] = id
+        // params[2] = api key
+
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        final String jsonFromServer;
+        try {
+            final Uri.Builder builder = new Uri.Builder();
+            builder.scheme("https").authority("api.themoviedb.org").appendPath("3")
+                    .appendPath("movie").appendPath(params[0]).appendPath("reviews")
+                    .appendQueryParameter("api_key", params[1]);
+
+            final URL url = new URL(builder.toString());
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            final InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            jsonFromServer = buffer.toString();
+        } catch (final IOException e) {
+            Log.e(LOG_TAG, "Error ", e);
+            // Can't parse data if it wasn't successfully retrieved.
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+
+        try {
+            return getDataFromJson(jsonFromServer);
+        } catch (final JSONException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    /**
+     * Converts the json string of movie data to an array of String trailers.
+     *
+     * @param trailersJsonStr the string of json
+     * @return a String array of trailers
+     * @throws JSONException if parsing string fails
+     */
+    public String[] getDataFromJson(final String trailersJsonStr) throws JSONException {
+        // These are the names of the JSON objects that need to be extracted.
+        final String OWM_RESULTS = "results";
+        final String OWM_KEY = "key";
+
+        final JSONObject trailersJson = new JSONObject(trailersJsonStr);
+        final JSONArray trailersArray = trailersJson.getJSONArray(OWM_RESULTS);
+
+        final String[] trailers = new String[trailersArray.length()];
+
+        for (int i = 0; i < trailersArray.length(); i++) {
+            final JSONObject movieObject = trailersArray.getJSONObject(i);
+
+            trailers[i] = movieObject.getString(OWM_KEY);
+        }
+
+        return trailers;
+    }
+}
